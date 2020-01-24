5f0053e1645ca8450a5ccfb8072852bd8df196a7
OtotheA <oscar@otothea.com>
2020-01-24 16:53:11 -0600

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -50,4 +50,5 @@ html_theme = 'classic'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+master_doc = 'index'
 source_suffix = ['.rst', '.md']
